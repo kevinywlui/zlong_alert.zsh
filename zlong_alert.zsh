f0d29ebc0d24fcb4339ae3fb4c95be5c76695548
kevin lui <kevinywlui@gmail.com>
2019-09-26 15:12:24 -0700

    Use notify-send only if it exists and has not been explicitly disabled

--- zlong_alert.zsh
+++ zlong_alert.zsh
@@ -4,9 +4,11 @@ zmodload zsh/datetime || return
 # Be sure we can actually set hooks
 autoload -Uz add-zsh-hook || return
 
+# Use notify-send if it exists and is not explicitly disabled
 if ! [[ -x "$(command -v notify-send)" ]]; then
-  echo 'Error: notify-send is not installed.' >&2
+    zlong_use_notify_send='false'
 fi
+(( ${+zlong_use_notify_send} )) || zlong_use_notify_send='true'
 
 # Define a long duration if needed
 (( ${+zlong_duration} )) || zlong_duration=15
@@ -24,7 +26,9 @@ zlong_alert_func() {
     local cmd=$1
     local secs=$2
     local ftime=$(printf '%dh:%dm:%ds\n' $(($secs / 3600)) $(($secs % 3600 / 60)) $(($secs % 60)))
-    notify-send "Done: $1" "Time: $ftime"
+    if [[ "$zlong_use_notify_send" == true ]]; then
+        notify-send "Done: $1" "Time: $ftime"
+    fi
     echo "\a"
 }
 
