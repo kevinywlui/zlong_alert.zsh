4a99d876ebef295bd99ec552bc964efd2d235c50
chris <chrisrhayden@gmail.com>
2020-06-24 03:54:17 -0700

    change the cut command to awk to not break on white space

--- zlong_alert.zsh
+++ zlong_alert.zsh
@@ -40,7 +40,7 @@ zlong_alert_pre() {
 zlong_alert_post() {
     local duration=$(($EPOCHSECONDS - $zlong_timestamp))
     local lasted_long=$(($duration - $zlong_duration))
-    local cmd_head=$(echo $zlong_last_cmd | cut -d ' ' -f 1)
+    local cmd_head=$(echo $zlong_last_cmd | awk '{printf $1}')
     if [[ $lasted_long -gt 0 && ! -z $zlong_last_cmd && ! $zlong_ignore_cmds =~ $cmd_head ]]; then
         zlong_alert_func $zlong_last_cmd duration
     fi
