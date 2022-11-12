ac5b0735163b4d6f0df0c1f20d3f8b1a3391cf0f
Fabien Delavier <fabien.delavier@linkt.fr>
2022-11-12 01:18:18 +0100

    Fix failed to compile regex

--- zlong_alert.zsh
+++ zlong_alert.zsh
@@ -84,7 +84,7 @@ zlong_alert_post() {
     local no_pfx
     while [[ -n "$last_cmd_no_pfx" && -z "$no_pfx" ]]; do
  	cmd_head="${last_cmd_no_pfx%% *}"
-	if [[ $zlong_ignore_pfxs =~ (^|[[:space:]])${cmd_head}([[:space:]]|$) ]]; then
+	if [[ $zlong_ignore_pfxs =~ (^|[[:space:]])${(q)cmd_head}([[:space:]]|$) ]]; then
 	    last_cmd_no_pfx="${last_cmd_no_pfx#* }"
 	else
 	    no_pfx=true
@@ -92,7 +92,7 @@ zlong_alert_post() {
     done
 
     # Notify only if delay > $zlong_duration and command not ignored
-    if [[ $lasted_long -gt 0 && ! -z $last_cmd_no_pfx && ! "$zlong_ignore_cmds" =~ (^|[[:space:]])${cmd_head}([[:space:]]|$) ]]; then
+    if [[ $lasted_long -gt 0 && ! -z $last_cmd_no_pfx && ! "$zlong_ignore_cmds" =~ (^|[[:space:]])${(q)cmd_head}([[:space:]]|$) ]]; then
         zlong_alert_func "$zlong_last_cmd" duration
     fi
     zlong_last_cmd=''
