812e24a1779487d669ead4c287d0dac7caf8b714
Fabien Delavier <fabien.delavier@linkt.fr>
2022-11-13 22:31:58 +0100

    Add quotes, replace zlong_timestamp init

--- zlong_alert.zsh
+++ zlong_alert.zsh
@@ -5,10 +5,10 @@ zmodload zsh/datetime || return
 autoload -Uz add-zsh-hook || return
 
 # Use value of zlong_use_notify_send if defined
-(( ${+zlong_use_notify_send} )) && zlong_internal_send_notifications=$zlong_use_notify_send
+(( ${+zlong_use_notify_send} )) && zlong_internal_send_notifications="$zlong_use_notify_send"
 
 # Use value of zlong_send_notifications if defined - This takes precedence over zlong_use_notify_send
-(( ${+zlong_send_notifications} )) && zlong_internal_send_notifications=$zlong_send_notifications
+(( ${+zlong_send_notifications} )) && zlong_internal_send_notifications="$zlong_send_notifications"
 
 # Disable notifications if both alerter and notify-send don't exist
 if ! ([[ -x "$(command -v notify-send)" ]] || [[ -x "$(command -v alerter)" ]]); then
@@ -33,15 +33,11 @@ fi
 # Define a custom message to display
 (( ${+zlong_message} )) || zlong_message='"Done: $cmd Time: $ftime"'
 
-# Need to set an initial timestamps otherwise, we'll be comparing an empty
-# string with an integer.
-zlong_timestamp=$EPOCHSECONDS
-
 # Define the alerting function, do the text processing here
 zlong_alert_func() {
-    local cmd=$1
-    local secs=$2
-    local ftime=$(printf '%dh:%dm:%ds\n' $(($secs / 3600)) $(($secs % 3600 / 60)) $(($secs % 60)))
+    local cmd="$1"
+    local secs="$2"
+    local ftime="$(printf '%dh:%dm:%ds\n' $(($secs / 3600)) $(($secs % 3600 / 60)) $(($secs % 60)))"
     if [[ "$zlong_internal_send_notifications" != false ]]; then
         # Find and use the correct notification command based on OS name
         if [[ "$OSTYPE" == "linux-gnu"* ]]; then
@@ -57,9 +53,9 @@ zlong_alert_func() {
 }
 
 zlong_alert_pre() {
-    zlong_last_cmd=$1
+    zlong_last_cmd="$1"
 
-    if [[ $zlong_ignorespace == 'true' && ${zlong_last_cmd:0:1} == [[:space:]] ]]; then
+    if [[ "$zlong_ignorespace" == 'true' && "${zlong_last_cmd:0:1}" == [[:space:]] ]]; then
         # set internal variables to nothing ignoring this command
         zlong_last_cmd=''
         zlong_timestamp=0
@@ -69,7 +65,7 @@ zlong_alert_pre() {
 }
 
 zlong_alert_post() {
-    local duration=$(($EPOCHSECONDS - $zlong_timestamp))
+    local duration=$(($EPOCHSECONDS - ${zlong_timestamp-$EPOCHSECONDS}))
     local lasted_long=$(($duration - $zlong_duration))
     local cmd_head
 
@@ -78,7 +74,7 @@ zlong_alert_post() {
     local no_pfx
     while [[ -n "$last_cmd_no_pfx" && -z "$no_pfx" ]]; do
  	cmd_head="${last_cmd_no_pfx%% *}"
-	if [[ $zlong_ignore_pfxs =~ (^|[[:space:]])${(q)cmd_head}([[:space:]]|$) ]]; then
+	if [[ "$zlong_ignore_pfxs" =~ "(^|[[:space:]])${(q)cmd_head}([[:space:]]|$)" ]]; then
 	    last_cmd_no_pfx="${last_cmd_no_pfx#* }"
 	else
 	    no_pfx=true
@@ -86,7 +82,7 @@ zlong_alert_post() {
     done
 
     # Notify only if delay > $zlong_duration and command not ignored
-    if [[ $lasted_long -gt 0 && ! -z $last_cmd_no_pfx && ! "$zlong_ignore_cmds" =~ (^|[[:space:]])${(q)cmd_head}([[:space:]]|$) ]]; then
+    if [[ $lasted_long -gt 0 && ! -z $last_cmd_no_pfx && ! "$zlong_ignore_cmds" =~ "(^|[[:space:]])${(q)cmd_head}([[:space:]]|$)" ]]; then
         zlong_alert_func "$zlong_last_cmd" duration
     fi
     zlong_last_cmd=''
